318b65f2ffe9b9e1e9b24af263e7b101bf326d34
Priyal Saxena <priyalsaxena66@gmail.com>
2021-11-07 17:52:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// function updateInput(value) {
+// 	document.getElementById("dateTime").value = value;
+// }
+
+function printDateTime() {
+	const dateInput = document.querySelector("#dateTime");
+	console.log(dateInput.value);
+}
